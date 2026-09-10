0fff984d3b273aa704cbedde8341cc2bbed6a1e5
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-09-10 13:17:52 +0530

    Step 19: cast_to_half_precision

--- model.py
+++ model.py
@@ -186,8 +186,14 @@ def estimate_checkpointing_memory_savings(batch_size, in_dim, hidden_dim, out_di
         saved_bytes=full_bytes-checkpoint_bytes
     )
 
-# Step 19 - cast_to_half_precision (not yet solved)
-# TODO: implement
+# Step 19 - cast_to_half_precision
+def cast_to_half_precision(values):
+    # TODO: Return a new dict mapping each key to its array converted to float16.
+    out = {}
+    for key in values:
+        out[key] = values[key].astype(np.float16)
+
+    return out
 
 # Step 20 - make_master_params (not yet solved)
 # TODO: implement
